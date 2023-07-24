63cc84e65dd9a83d5405579ed56345907748fb64
Xiaoyun <yangx232@gmail.com>
2023-07-23 21:45:44 -0400

    create RandomWord child widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         ),
         home: MyHomePage(),
       ),
@@ -52,6 +52,36 @@ class RandomWordModel extends ChangeNotifier {
   }
 }
 
+class RandomWord extends StatelessWidget {
+  const RandomWord({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(80),
+        child: Consumer<RandomWordModel>(
+            builder: (context, randomWordState, child) => Column(children: [
+                  Text(randomWordState.current.asLowerCase, style: style),
+                  ElevatedButton(
+                    onPressed: () {
+                      randomWordState.getNext();
+                    },
+                    child: Text('Next Random Word Pair State'),
+                  ),
+                ])),
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -60,16 +90,7 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Consumer<RandomWordModel>(
-                builder: (context, randomWordState, child) => Column(children: [
-                      Text(randomWordState.current.asLowerCase),
-                      ElevatedButton(
-                        onPressed: () {
-                          randomWordState.getNext();
-                        },
-                        child: Text('Next Random Word Pair State'),
-                      ),
-                    ])),
+            RandomWord(),
             Consumer<ClockModel>(
                 builder: (context, clockState, child) => (Column(children: [
                       Text(clockState.current),
